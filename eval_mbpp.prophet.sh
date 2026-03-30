bfbbfd4d18f199b3f04f740449a75f1e4a0adb38
mingyu cao <m.cao@surrey.ac.uk>
2026-03-30 10:08:34 +0800

    add

---
+++ eval_mbpp.prophet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name="soar_eval"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4                # 请求2块GPU
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate soar
+
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+export CUDA_VISIBLE_DEVICES=0
+export HF_ALLOW_CODE_EVAL="1"
+#  --limit 256
+
+accelerate launch eval_llada.py \
+  --tasks mbpp \
+  --confirm_run_unsafe_code --model llada_dist \
+  --output_path evals_results/prophet_soar/mbpp-ns0-${length} --log_samples \
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',enable_early_exit=true,enable_soar=false,constraints_text="200:The|201:answer|202:is",gen_length=256,steps=256,block_length=32,answer_length=5 \
