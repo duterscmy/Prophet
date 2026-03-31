a1b9a9bc39a6d23c7b0712461031e3e986a22672
mingyu cao <m.cao@surrey.ac.uk>
2026-03-31 11:57:33 +0800

    add

--- eval_human_eval.sh
+++ eval_human_eval.sh
@@ -16,7 +16,7 @@ export HF_DATASETS_OFFLINE=0
 export CUDA_VISIBLE_DEVICES=0
 export HF_ALLOW_CODE_EVAL="1"
 #  --limit 256
-
+length=256
 accelerate launch eval_llada.py \
   --tasks humaneval \
   --confirm_run_unsafe_code --model llada_dist \
