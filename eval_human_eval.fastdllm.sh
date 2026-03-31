6d6902bfb3980a72451663d2477c03c68f67e7e2
mingyu cao <m.cao@surrey.ac.uk>
2026-03-31 11:09:02 +0800

    add

--- eval_human_eval.fastdllm.sh
+++ eval_human_eval.fastdllm.sh
@@ -23,13 +23,13 @@ block_length=32
 steps=256
 mkdir -p logs
 
-accelerate launch eval_llada.fastdllm.py --tasks humaneval \
+TORCHINDUCTOR_DISABLED=1 accelerate launch eval_llada.fastdllm.py --tasks humaneval \
   --confirm_run_unsafe_code --model llada_dist \
   --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${steps},block_length=${block_length},use_cache=True,threshold=0.9,show_speed=True \
   --output_path evals_results/cache_parallel/humaneval-ns0-${length} --log_samples &> logs/cache_parallel-humaneval-ns0-${length}.log
 
 
-accelerate launch eval_llada.fastdllm.py --tasks mbpp \
+TORCHINDUCTOR_DISABLED=1 accelerate launch eval_llada.fastdllm.py --tasks mbpp \
   --confirm_run_unsafe_code --model llada_dist \
   --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${steps},block_length=${block_length},use_cache=True,threshold=0.9,show_speed=True \
   --output_path evals_results/cache_parallel/mbpp-ns0-${length} --log_samples &> logs/cache_parallel-mbpp-ns0-${length}.log
