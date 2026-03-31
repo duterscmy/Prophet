7a6efd47c8438aedd11aea7c4807d0edf1b5f5c7
mingyu cao <m.cao@surrey.ac.uk>
2026-03-31 11:46:22 +0800

    add

--- eval_gsm8k.soar.sh
+++ eval_gsm8k.soar.sh
@@ -15,9 +15,10 @@ export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
 export CUDA_VISIBLE_DEVICES=0
 export HF_ALLOW_CODE_EVAL="1"
+length=512
 
 accelerate launch eval_llada.py \
   --tasks gsm8k_cot_zeroshot \
   --confirm_run_unsafe_code --model llada_dist \
   --output_path evals_results/soar095/gsm8k_cot_zeroshot-ns0-${length} --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',enable_early_exit=false,enable_soar=true,constraints_text="200:The|201:answer|202:is",gen_length=256,steps=256,block_length=32,answer_length=5 &> logs/soar095-gsm8k_cot_zeroshot-ns0-${length}.log
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',enable_early_exit=false,enable_soar=true,constraints_text="200:The|201:answer|202:is",gen_length=${length},steps=${length},block_length=32,answer_length=5 &> logs/soar095-gsm8k_cot_zeroshot-ns0-${length}.log
