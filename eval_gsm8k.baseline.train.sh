d2eefe3fb28e5eb14daf5e066cd2b09b34174512
mingyu cao <m.cao@surrey.ac.uk>
2026-05-26 23:06:27 +0100

    add

---
+++ eval_gsm8k.baseline.train.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name="gsm8k_eval"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+source ~/.bashrc
+conda activate soar
+
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+mkdir -p logs
+mkdir -p evals_results/baseline
+
+length=256
+block=32
+
+accelerate launch --num_processes 4 eval_llada.auto_thresh.py \
+  --tasks gsm8k_cot_zeroshot_train \
+  --model llada_dist \
+  --output_path evals_results/baseline/gsm8k_train_standard_len${length}_block${block}_4gpu \
+  --log_samples \
+  --limit 1000 \
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false,constraints_text="200:The|201:answer|202:is" \
+  &> logs/baseline-gsm8k_train_standard-len${length}-block${block}-4gpu.log
