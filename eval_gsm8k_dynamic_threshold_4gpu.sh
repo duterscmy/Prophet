d7f0c0da6f76f42fc2e37841907f945d7084dc67
mingyu cao <m.cao@surrey.ac.uk>
2026-05-26 22:39:44 +0100

    add

--- eval_gsm8k_dynamic_threshold_4gpu.sh
+++ eval_gsm8k_dynamic_threshold_4gpu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name="std_eval"
+#SBATCH --job-name="p50_gsm8k"
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
 #SBATCH --time=24:00:00
 #SBATCH -o slurm.%j.%N.out
 #SBATCH -e slurm.%j.%N.err
@@ -12,18 +12,23 @@ conda activate soar
 
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 mkdir -p logs
-mkdir -p evals_results/baseline
+mkdir -p evals_results/auto_thresh
 
 length=256
 block=32
+max_threshold=0.95
+min_threshold=0.05
+default_threshold=0.95
 
-accelerate launch eval_llada.py \
+threshold_json="token_threshold_stats/token_threshold_p05.json"
+
+accelerate launch --num_processes 4 eval_llada.auto_thresh.py \
   --tasks gsm8k_cot_zeroshot \
   --model llada_dist \
-  --output_path evals_results/baseline/gsm8k_standard_len${length}_block${block} \
+  --output_path evals_results/auto_thresh/gsm8k_dynamic_p05_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
   --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false,constraints_text="200:The|201:answer|202:is" \
-  &> logs/baseline-gsm8k_standard-len${length}-block${block}.log
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_dynamic_threshold=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
+  &> logs/gsm8k_dynamic_p05_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
