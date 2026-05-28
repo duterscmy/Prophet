d1aec45be48f3bd083b610b836d8d2169751324d
mingyu cao <m.cao@surrey.ac.uk>
2026-05-29 00:58:50 +0100

    add

--- eval_gsm8k_adaptive_parallel_4gpu.sh
+++ eval_gsm8k_adaptive_parallel_4gpu.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name="ap_gsm8k"
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:4
+#SBATCH --job-name=eval_gsm8k_adaptive
 #SBATCH --time=24:00:00
-#SBATCH -o slurm.%j.%N.out
-#SBATCH -e slurm.%j.%N.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=96G
+#SBATCH --partition=a100
 
 source ~/.bashrc
-conda activate soar
-
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
-# export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 mkdir -p logs
 mkdir -p evals_results/adaptive_parallel
@@ -21,10 +21,10 @@ length=256
 block=32
 threshold=0.95
 
-accelerate launch --num_processes 4 eval_llada.auto_thresh.py \
+accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --tasks gsm8k_cot_zeroshot \
   --model llada_dist \
   --output_path evals_results/adaptive_parallel/gsm8k_adaptive_parallel_len${length}_block${block}_thr${threshold} \
   --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=true,confidence_threshold=${threshold},min_parallel_tokens=1 \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=true,confidence_threshold=${threshold},min_parallel_tokens=1 \
   &> logs/gsm8k_adaptive_parallel_len${length}_block${block}_thr${threshold}.log
