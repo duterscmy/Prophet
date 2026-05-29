9012655af2513cbd0ac7aad24c90e58940c5a71d
mingyu cao <m.cao@surrey.ac.uk>
2026-05-29 12:00:29 +0100

    add

--- eval_math500.baseline.sh
+++ eval_math500.baseline.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name="math_std_eval"
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:4
+#SBATCH --job-name=eval_gsm8k_base
 #SBATCH --time=24:00:00
-#SBATCH -o slurm.%j.%N.out
-#SBATCH -e slurm.%j.%N.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=96G
+#SBATCH --partition=a100
 
 source ~/.bashrc
-conda activate soar
-
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-
 mkdir -p logs
 mkdir -p evals_results/baseline
 
@@ -21,11 +20,11 @@ length=256
 block=32
 
 # Note: eval_llada.py loads the model with torch_dtype=torch.bfloat16 internally.
-accelerate launch --num_processes 4 eval_llada.auto_thresh.py \
+accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --tasks minerva_math500 \
   --model llada_dist \
   --num_fewshot 0 \
   --output_path evals_results/baseline/math500_standard_len${length}_block${block}_4gpu_bf16 \
   --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false \
-  &> logs/baseline-math500_standard-len${length}-block${block}-4gpu-bf16.log
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false \
+  &> logs/math500_standard-len${length}-block${block}.log
