cc4bdac06e0a760b154847c896bb6ae34a1e513f
mingyu cao <m.cao@surrey.ac.uk>
2026-05-29 01:01:58 +0100

    add

--- eval_gsm8k.baseline.sh
+++ eval_gsm8k.baseline.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name="gsm8k_eval"
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:4
+#SBATCH --job-name=eval_gsm8k_base
 #SBATCH --time=24:00:00
-#SBATCH -o slurm.%j.%N.out
-#SBATCH -e slurm.%j.%N.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=96G
+#SBATCH --partition=a100
 
 source ~/.bashrc
-conda activate soar
-
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-
 mkdir -p logs
 mkdir -p evals_results/baseline
 
 length=256
 block=32
 
-accelerate launch --num_processes 4 eval_llada.auto_thresh.py \
+accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --tasks gsm8k_cot_zeroshot \
   --model llada_dist \
   --output_path evals_results/baseline/gsm8k_standard_len${length}_block${block}_4gpu \
   --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false,constraints_text="200:The|201:answer|202:is" \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false \
   &> logs/baseline-gsm8k_standard-len${length}-block${block}-4gpu.log
