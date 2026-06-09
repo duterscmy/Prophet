c269cc25369d970c10e15c4d8dedf62851b60590
mingyu cao <m.cao@surrey.ac.uk>
2026-06-09 23:56:01 +0100

    add

--- eval_human_eval_dynamic.global.sh
+++ eval_human_eval_dynamic.global.sh
@@ -36,7 +36,7 @@ accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --confirm_run_unsafe_code \
   --model llada_dist \
   --num_fewshot 0 \
-  --output_path evals_results/dynamic_threshold/humaneval_dynamic_threshold_from_global_v2_c${correct_ratio}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
+  --output_path evals_results/dynamic_threshold/humaneval_dynamic_threshold_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
   --log_samples \
   --model_args model_path='/lus/lfs1aip2/projects/public/u6nc/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=true,use_dynamic_threshold=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
-  &> logs/humaneval_dynamic_threshold_from_global_v2_c${correct_ratio}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
+  &> logs/humaneval_dynamic_threshold_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
