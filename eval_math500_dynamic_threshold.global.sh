03e0ebbf89d5c62a7c0942374b694c54df27cfd9
mingyu cao <m.cao@surrey.ac.uk>
2026-06-09 18:42:44 +0100

    add

--- eval_math500_dynamic_threshold.global.sh
+++ eval_math500_dynamic_threshold.global.sh
@@ -19,13 +19,13 @@ mkdir -p evals_results/auto_thresh
 
 length=256
 block=32
-correct_ratio=99.5
+correct_ratio=100.0
 max_threshold=0.90
 min_threshold=0.01
 default_threshold=$max_threshold
 min_count=200
 min_accepted=100
-threshold_json="token_threshold_on_trainset/global_token_threshold_grid_p${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}.json"
+threshold_json="token_threshold_on_trainset/global_v2_token_threshold_grid_p${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}.json"
 
 ls $threshold_json || (echo "Threshold json file not found: ${threshold_json}" && exit 1)
 
@@ -33,7 +33,7 @@ accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --tasks minerva_math500 \
   --model llada_dist \
   --num_fewshot 0 \
-  --output_path evals_results/auto_thresh/math500_dynamic_from_global_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
+  --output_path evals_results/auto_thresh/math500_dynamic_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
   --log_samples \
   --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_dynamic_threshold=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
-  &> logs/math500_dynamic_from_global_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
+  &> logs/math500_dynamic_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
