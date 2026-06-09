459a5cf66a1cbc73016b9f7da39b596d07be3768
mingyu cao <m.cao@surrey.ac.uk>
2026-06-10 00:13:13 +0100

    add

--- eval_math500_dynamic_threshold.global.sh
+++ eval_math500_dynamic_threshold.global.sh
@@ -38,5 +38,5 @@ accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --num_fewshot 0 \
   --output_path evals_results/auto_thresh/math500_dynamic_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
   --log_samples \
-  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_dynamic_threshold=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6nc/mingyu/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_dynamic_threshold=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
   &> logs/math500_dynamic_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
