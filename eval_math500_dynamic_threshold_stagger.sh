1407d2926093ee160d42e50ac68fcf770eafdfdb
mingyu cao <m.cao@surrey.ac.uk>
2026-06-16 23:48:12 +0100

    add

--- eval_math500_dynamic_threshold_stagger.sh
+++ eval_math500_dynamic_threshold_stagger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=eval_math500_dynamic
-#SBATCH --time=24:00:00
+#SBATCH --job-name=eval_math500_stagger
+#SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
@@ -13,22 +13,20 @@ conda activate ttrl_env
 cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
-export HF_HOME=/projects/u6nc/public/mingyu/hf_cache
-export HF_HUB_CACHE=$HF_HOME/hub
-export HF_DATASETS_CACHE=$HF_HOME/datasets
 
 mkdir -p logs
-mkdir -p evals_results/auto_thresh
+mkdir -p evals_results/auto_thresh_stagger
 
 length=256
 block=32
-correct_ratio=99.5
+correct_ratio=99.5 #99 99.5 99.7 100
 max_threshold=0.90
-min_threshold=0.01
+min_threshold=0.05
 default_threshold=$max_threshold
+
 min_count=200
 min_accepted=100
-threshold_json="token_threshold_on_trainset/math_token_threshold_grid_p${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}.json"
+threshold_json="token_threshold_on_trainset/global_v2_token_threshold_grid_p${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}.json"
 
 ls $threshold_json || (echo "Threshold json file not found: ${threshold_json}" && exit 1)
 
@@ -36,7 +34,7 @@ accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --tasks minerva_math500 \
   --model llada_dist \
   --num_fewshot 0 \
-  --output_path evals_results/auto_thresh/math500_dynamic_from_train_enablestagger_aware_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
+  --output_path evals_results/auto_thresh_stagger/math500_stagger_dynamic_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
   --log_samples \
-  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_dynamic_threshold=true,enable_stagger_aware=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
-  &> logs/math500_dynamic_from_train_enablestagger_aware_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',enable_stagger_aware=true,gen_length=${length},steps=${length},block_length=${block},use_dynamic_threshold=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
+  &> logs/math500_stagger_dynamic_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
