9924b5daae77af6938ec6ee90e7194cf7b894c3d
mingyu cao <m.cao@surrey.ac.uk>
2026-06-16 22:44:17 +0100

    add

--- eval_math500.prophet.sh
+++ eval_math500.prophet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=eval_math500_soar
+#SBATCH --job-name=eval_math_prophet
 #SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -13,16 +13,16 @@ conda activate ttrl_env
 cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
-export CUDA_VISIBLE_DEVICES=0
-export HF_ALLOW_CODE_EVAL="1"
-
+#  --limit 256
 length=256
+
+# constraints_text="200:The|201:answer|202:is"
 
 accelerate launch eval_llada.py \
   --tasks minerva_math500 \
   --model llada_dist \
   --num_fewshot 0 \
   --log_samples \
-  --output_path evals_results/soar/math500-ns0-${length}\
-  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',enable_early_exit=false,enable_soar=true,gen_length=${length},steps=${length},block_length=32 \
-  &> logs/soar-math500-ns0-${length}.log
+  --output_path evals_results/prophet/math500-ns0-${length} \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',enable_early_exit=true,enable_soar=false,gen_length=256,steps=256,block_length=32,answer_length=5 \
+  &> logs/prophet-math500-ns0-${length}.log
--- eval_math500.wino.sh
+++ eval_math500.wino.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=eval_gsm8k_soar
-#SBATCH --time=3:00:00
+#SBATCH --job-name=eval_math500_wino
+#SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
@@ -15,14 +15,14 @@ export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
 export CUDA_VISIBLE_DEVICES=0
 export HF_ALLOW_CODE_EVAL="1"
-
+#  --limit 256
 length=256
 
-accelerate launch eval_llada.py \
-  --tasks gsm8k_cot_zeroshot \
+accelerate launch eval_llada.wino.py \
+  --tasks minerva_math500 \
   --model llada_dist \
   --num_fewshot 0 \
   --log_samples \
-  --output_path evals_results/soar/gsm8k_cot_zeroshot-ns0-${length}\
-  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',enable_early_exit=false,enable_soar=true,gen_length=${length},steps=${length},block_length=32 \
-  &> logs/soar-gsm8k_cot_zeroshot-ns0-${length}.log
+  --output_path evals_results/wino/math500-ns0-${length} \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=32  \
+  &> logs/wino-math500-ns0-${length}.log
