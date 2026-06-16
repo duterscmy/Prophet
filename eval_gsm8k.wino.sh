05eee42b40f5e8de4043cb5f3b6e6e372858600f
mingyu cao <m.cao@surrey.ac.uk>
2026-06-16 22:20:53 +0100

    add

--- eval_gsm8k.wino.sh
+++ eval_gsm8k.wino.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name="soar_eval"
+#SBATCH --job-name=eval_gsm8k_wino
+#SBATCH --time=3:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1                # 请求2块GPU
-#SBATCH --time=24:00:00
-#SBATCH -o slurm.%j.%N.out
-#SBATCH -e slurm.%j.%N.err
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --partition=a100
 
-### 激活conda环境
-source ~/.bashrc # 你的环境名
-conda activate soar
-
+source ~/.bashrc
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
 export CUDA_VISIBLE_DEVICES=0
+export HF_ALLOW_CODE_EVAL="1"
 #  --limit 256
 length=256
 
 accelerate launch eval_llada.wino.py \
   --tasks gsm8k_cot_zeroshot \
   --model llada_dist \
-  --output_path evals_results/wino/gsm8kns0-${length} --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct-wino',enable_early_exit=false,enable_soar=false,gen_length=${length},steps=${length},block_length=32  #&> logs/wino-gsm8k_cot_zeroshot-ns0-${length}.log
+  --num_fewshot 0 \
+  --log_samples \
+  --output_path evals_results/wino/gsm8kns0-${length} \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=32  &> logs/wino-gsm8k_cot_zeroshot-ns0-${length}.log
