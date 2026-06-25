29af7b5835bb682cb84351c9360591dace04c00b
mingyu cao <m.cao@surrey.ac.uk>
2026-06-25 23:37:17 +0100

    add

---
+++ eval_human_eval.dynamic_dllm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=eval_humaneval_dynamic_dllm
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --partition=a100
+
+source ~/.bashrc
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+export CUDA_VISIBLE_DEVICES=0
+export HF_ALLOW_CODE_EVAL="1"
+
+length=256
+
+accelerate launch eval_llada.py \
+  --tasks humaneval \
+  --confirm_run_unsafe_code \
+  --model llada_dist \
+  --log_samples \
+  --output_path evals_results/dynamic_dllm/humaneval-ns0-${length} \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',enable_dynamic_dllm=true,gen_length=256,steps=256,block_length=32 \
+  &> logs/dynamic_dllm-humaneval-ns0-${length}.log
---
+++ eval_math500.dynamic_dllm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=eval_math_dynamic_dllm
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --partition=a100
+
+source ~/.bashrc
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+
+length=256
+
+# constraints_text="200:The|201:answer|202:is"
+
+accelerate launch eval_llada.py \
+  --tasks minerva_math500 \
+  --model llada_dist \
+  --num_fewshot 0 \
+  --log_samples \
+  --output_path evals_results/dynamic_dllm/math500-ns0-${length} \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',enable_dynamic_dllm=true,gen_length=256,steps=256,block_length=32 \
+  &> logs/dynamic_dllm -math500-ns0-${length}.log
