5a8241e3a2671f564233841ece07ef60048b42b1
mingyu cao <m.cao@surrey.ac.uk>
2026-07-02 18:37:32 +0100

    add

--- eval_wmt_de_en.baseline.sh
+++ eval_wmt_de_en.baseline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=eval_gsm8k_base
-#SBATCH --time=24:00:00
+#SBATCH --job-name=eval_wmt_de_en_baseline
+#SBATCH --time=6:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
@@ -16,13 +16,13 @@ export HF_DATASETS_OFFLINE=0
 mkdir -p logs
 mkdir -p evals_results/baseline
 
-length=256
+length=64
 block=32
 
 accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
-  --tasks gsm8k_cot_zeroshot \
+  --tasks wmt16-de-en \
   --model llada_dist \
-  --output_path evals_results/baseline/gsm8k_standard_len${length}_block${block}_4gpu \
+  --output_path evals_results/baseline/wmt_de_en_baseline_len${length}_block${block}_4gpu \
   --log_samples \
   --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false \
-  &> logs/baseline-gsm8k_standard-len${length}-block${block}-4gpu.log
+  &> logs/baseline-wmt_de_en_baseline-len${length}-block${block}-4gpu.log
