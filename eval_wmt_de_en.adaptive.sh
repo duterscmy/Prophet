d53f11f743d8de1d7fc0c1522e756cb289292d28
mingyu cao <m.cao@surrey.ac.uk>
2026-07-02 18:35:12 +0100

    add

---
+++ eval_wmt_de_en.adaptive.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=eval_wmt_de_en_adaptive
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --partition=a100
+
+source ~/.bashrc
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+
+mkdir -p logs
+mkdir -p evals_results/adaptive_parallel
+
+length=64
+block=32
+threshold=0.80
+
+accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
+  --tasks wmt16-de-en \
+  --model llada_dist \
+  --output_path evals_results/adaptive_parallel/wmt_de_en_adaptive_len${length}_block${block}_thr${threshold} \
+  --log_samples \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=true,confidence_threshold=${threshold},min_parallel_tokens=1 \
+  &> logs/wmt_de_en_adaptive_len${length}_block${block}_thr${threshold}.log
---
+++ eval_wmt_de_en.baseline.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=eval_gsm8k_base
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=96G
+#SBATCH --partition=a100
+
+source ~/.bashrc
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/prophet
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+mkdir -p logs
+mkdir -p evals_results/baseline
+
+length=256
+block=32
+
+accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
+  --tasks gsm8k_cot_zeroshot \
+  --model llada_dist \
+  --output_path evals_results/baseline/gsm8k_standard_len${length}_block${block}_4gpu \
+  --log_samples \
+  --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=false,use_dynamic_threshold=false \
+  &> logs/baseline-gsm8k_standard-len${length}-block${block}-4gpu.log
