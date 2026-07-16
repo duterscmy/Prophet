c456b5356322581fbffb03021e5615e96c6594b2
mingyu cao <m.cao@surrey.ac.uk>
2026-07-16 23:59:02 +0800

    add

--- eval_human_eval_dynamic.global.sh
+++ eval_human_eval_dynamic.global.sh
@@ -23,12 +23,12 @@ mkdir -p evals_results/dynamic_threshold
 length=256
 block=32
 correct_ratio=99.5  #99.0/99.5/100.0
-max_threshold=0.80
+max_threshold=0.90
 min_threshold=0.01
 default_threshold=$max_threshold
 min_count=200
 min_accepted=100
-threshold_json="token_threshold_on_trainset/global_v2_token_threshold_grid_p${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}.json"
+threshold_json="token_threshold_on_trainset/global_v3_token_threshold_grid_p${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}.json"
 
 
 # Note: eval_llada.py loads the model with torch_dtype=torch.bfloat16 internally.
@@ -37,7 +37,7 @@ accelerate launch --num_processes 1 eval_llada.auto_thresh.py \
   --confirm_run_unsafe_code \
   --model llada_dist \
   --num_fewshot 0 \
-  --output_path evals_results/dynamic_threshold/humaneval_dynamic_threshold_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
+  --output_path evals_results/dynamic_threshold/humaneval_dynamic_threshold_from_global_v3_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold} \
   --log_samples \
   --model_args model_path='/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct',gen_length=${length},steps=${length},block_length=${block},use_adaptive_parallel=true,use_dynamic_threshold=true,dynamic_threshold_json=${threshold_json},max_threshold=${max_threshold},min_threshold=${min_threshold},default_threshold=${default_threshold},min_parallel_tokens=1 \
-  &> logs/humaneval_dynamic_threshold_from_global_v2_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
+  &> logs/humaneval_dynamic_threshold_from_global_v3_c${correct_ratio}_mincount${min_count}_minaccepted${min_accepted}_len${length}_block${block}_maxthr${max_threshold}_minthr${min_threshold}.log
